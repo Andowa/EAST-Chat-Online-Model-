30671f66a2b94f10f3ec61b430616f67bb7d89e8
Andowa <49613758+Andowa@users.noreply.github.com>
2023-04-25 13:40:48 +0800

    Update de.ts

--- app/locales/de.ts
+++ app/locales/de.ts
@@ -11,7 +11,7 @@ const de: LocaleType = {
     ChatItemCount: (count: number) => `${count} Nachrichten`,
   },
   Chat: {
-    SubTitle: (count: number) => `${count} Nachrichten mit ChatGPT`,
+    SubTitle: (count: number) => `${count} Nachrichten mit EAST-Chat`,
     Actions: {
       ChatList: "Zur Chat-Liste gehen",
       CompressedHistory: "Komprimierter Gedächtnis-Prompt",
@@ -37,7 +37,7 @@ const de: LocaleType = {
     Copy: "Alles kopieren",
     Download: "Herunterladen",
     MessageFromYou: "Deine Nachricht",
-    MessageFromChatGPT: "Nachricht von ChatGPT",
+    MessageFromChatGPT: "Nachricht von EAST-Chat",
   },
   Memory: {
     Title: "Gedächtnis-Prompt",
